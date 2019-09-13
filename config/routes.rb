55daf956d2684cb1f1b876df299966e8966907bf
mathieu bouchant <matbcht@gmail.com>
2019-09-13 14:32:27 +0200

    fin ex2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'questions/answer'
-  get 'questions/ask'
+  get 'ask',    to: 'questions#ask',    as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
 end
-
